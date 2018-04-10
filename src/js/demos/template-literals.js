7a567e454cd0d1f6019945aef03ecfec9add23f4
Quangdao Nguyen <quangdaonguyen@graydientcreative.com>
2018-04-10 10:07:23 -0500

    Somehow forgot template literals

---
+++ src/js/demos/template-literals.js
@@ -0,0 +1,16 @@
+const id = '12354';
+
+const apiKey = 'sdf76fsdf79v79d7f87fs9df';
+
+const url = `https://www.example.com/api/stuff?apikey=${apiKey}&id=${id}`;
+
+console.log(url);
+
+const template = `
+<div>
+	<h1>Hello World</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deserunt earum laboriosam laudantium molestiae necessitatibus numquam quasi sunt! Dolorum excepturi, fuga maxime minima numquam reiciendis?</p>
+</div>
+`;
+
+console.log(template);
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,7 +1,8 @@
 //import './demos/const-let';
 //import './demos/arrow-shorthands';
+//import './demos/template-literals';
 //import './demos/destructuring';
 //import './demos/rest';
 //import './demos/class';
 
-import './demos/imports';
+//import './demos/imports';
