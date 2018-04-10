55ae7dd09d5677877cc56e4f47424e670cdfdaad
Quangdao Nguyen <quangdaonguyen@graydientcreative.com>
2018-04-10 10:11:06 -0500

    Block scoping

--- src/js/demos/const-let.js
+++ src/js/demos/const-let.js
@@ -4,6 +4,17 @@ let dork = 'gork';
 
 // doof = 'carrot' //<- Error
 
+
 dork = 'carrot';
 
 console.log(doof, dork);
+
+let y = 100;
+
+if (true) {
+	let x = 123;
+	y = 600
+}
+
+ //x //<- Undefined
+ //y //<- 600
